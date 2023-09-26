debf258319f3770523ab2464b9ab21c4813fb5d0
nalle631 <malteolofsson97@gmail.com>
2023-09-26 12:20:27 +0200

    added img folder and mesh image

--- pkg/protocol/main.go
+++ pkg/protocol/main.go
@@ -5,27 +5,49 @@ import (
 
 	"github.com/Domilz/d7017e-mesh-network/pkg/protocol/pb"
 	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
 
 	mockReading := &pb.Reading{
-		TagId:    "MOCKTAG",
+		TagId:    "00-B0-D0-63-C2-27",
 		DeviceId: "321",
-		Rssi:     69,
+		Rssi:     5,
 		Ts: &timestamp.Timestamp{
 			Seconds: timestamppb.Now().Seconds,
 		},
 	}
+	// mockReading2 := &pb.Reading{
+	// 	TagId:    "00-B0-D0-63-C2-28",
+	// 	DeviceId: "321",
+	// 	Rssi:     5,
+	// 	Ts: &timestamp.Timestamp{
+	// 		Seconds: timestamppb.Now().Seconds,
+	// 	},
+	// }
+	// mockReading3 := &pb.Reading{
+	// 	TagId:    "10-B0-D0-63-C2-26",
+	// 	DeviceId: "321",
+	// 	Rssi:     5,
+	// 	Ts: &timestamp.Timestamp{
+	// 		Seconds: timestamppb.Now().Seconds,
+	// 	},
+	// }
 
 	mockState := &pb.State{
-		TagId: "666",
-		Readings: map[string]*pb.Reading{
-			mockReading.TagId: mockReading,
-		},
+		TagId:    "666",
+		Readings: map[string]*pb.Reading{},
 	}
 
-	fmt.Println("Testing a mock state: ", mockState)
+	//fmt.Println("Testing a mock state: ", mockState)
+	_, err := proto.Marshal(mockReading)
+	if err != nil {
+		fmt.Println("marshall error", err)
+	}
+
+	fmt.Println(mockState.Readings)
+	fmt.Println(proto.Size(mockState))
 
 }
