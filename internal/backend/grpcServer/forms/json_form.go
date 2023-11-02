4de15bf92a1465dc8f875e0058cbc137fb19b6b4
ivargruneau <ivar.gruneau@gmail.com>
2023-11-02 10:10:06 +0100

    Made changes according to requested changes
    Added message to error
    Removed Struct name from structs

--- internal/backend/grpcServer/forms/json_form.go
+++ internal/backend/grpcServer/forms/json_form.go
@@ -1,83 +1,84 @@
 package forms
 
 type ReferencePointForm struct {
-	Timestamp     TimeStruct       `json:"timestamp"`
-	CorrelationId string           `json:"correlationId"`
-	Operation     string           `json:"operation"`
-	Operands      []OperandsStruct `json:"operands"`
-	Error         ErrorStruct      `json:"error"`
+	Timestamp     Time       `json:"timestamp"`
+	CorrelationId string     `json:"correlationId"`
+	Operation     string     `json:"operation"`
+	Operands      []Operands `json:"operands"`
+	Error         Error      `json:"error"`
 }
 
 type RssiForm struct {
-	Readings []ReadingStruct `json:"readings"`
+	Readings []Reading `json:"readings"`
 }
 
 type XYZForm struct {
-	X           float32             `json:"x"`
-	Y           float32             `json:"y"`
-	Z           float32             `json:"z"`
-	Accuracy    int                 `json:"accuracy"`
-	Tag_id      string              `json:"tag_id"`
-	Chain_delay []Chain_delayStruct `json:"chain_delay"`
+	X           float32       `json:"x"`
+	Y           float32       `json:"y"`
+	Z           float32       `json:"z"`
+	Accuracy    int           `json:"accuracy"`
+	Tag_id      string        `json:"tag_id"`
+	Chain_delay []Chain_delay `json:"chain_delay"`
 }
-type TimeStruct struct {
+type Time struct {
 	Seconds int `json:"seconds"`
 	Nanos   int `json:"nanos"`
 }
 
-type OperandsStruct struct {
-	Uuid             string                 `json:"uuid"`
-	RpId             string                 `json:"rpId"`
-	Name             string                 `json:"name"`
-	Description      string                 `json:"description"`
-	CreateTime       TimeStruct             `json:"createTime"`
-	UpdateTime       TimeStruct             `json:"updateTime"`
-	Location         XYZStruct              `json:"location"`
-	Type             string                 `json:"type"`
-	WifiProperties   WifiPropertiesStruct   `json:"wifiProperties"`
-	BLEProperties    BLEPropertiesStruct    `json:"bleProperties"`
-	LteProperties    LtePropertiesStruct    `json:"lteProperties"`
-	Vendor           string                 `json:"vendor"`
-	VendorProperties VendorPropertiesStruct `json:"vendorProperties"`
+type Operands struct {
+	Uuid             string           `json:"uuid"`
+	RpId             string           `json:"rpId"`
+	Name             string           `json:"name"`
+	Description      string           `json:"description"`
+	CreateTime       Time             `json:"createTime"`
+	UpdateTime       Time             `json:"updateTime"`
+	Location         XYZ              `json:"location"`
+	Type             string           `json:"type"`
+	WifiProperties   WifiProperties   `json:"wifiProperties"`
+	BLEProperties    BLEProperties    `json:"bleProperties"`
+	LteProperties    LteProperties    `json:"lteProperties"`
+	Vendor           string           `json:"vendor"`
+	VendorProperties VendorProperties `json:"vendorProperties"`
 }
 
-type XYZStruct struct {
+type XYZ struct {
 	X float32 `json:"x"`
 	Y float32 `json:"y"`
 	Z float32 `json:"z"`
 }
 
-type WifiPropertiesStruct struct {
+type WifiProperties struct {
 	Online         bool   `json:"online"`
 	LocationStatus string `json:"locationStatus"`
 	IP             string `json:"ip"`
 }
 
-type BLEPropertiesStruct struct {
+type BLEProperties struct {
 	Active            bool `json:"active"`
 	BatteryPercentage int  `json:"batteryPercentage"`
 }
 
-type LtePropertiesStruct struct {
+type LteProperties struct {
 }
 
-type VendorPropertiesStruct struct {
+type VendorProperties struct {
 }
 
-type ErrorStruct struct {
-	Code int `json:"code"`
+type Error struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
 }
 
-type ReadingStruct struct {
-	Rp_id       string              `json:"rp_id"`
-	Rssi        int                 `json:"rssi"`
-	Tag_id      string              `json:"tag_id"`
-	Type        string              `json:"BLE"`
-	Chain_delay []Chain_delayStruct `json:"chain_delay"`
+type Reading struct {
+	Rp_id       string        `json:"rp_id"`
+	Rssi        int           `json:"rssi"`
+	Tag_id      string        `json:"tag_id"`
+	Type        string        `json:"BLE"`
+	Chain_delay []Chain_delay `json:"chain_delay"`
 }
 
-type Chain_delayStruct struct {
-	Name     string     `json:"tagbackend"`
-	Sent     TimeStruct `json:"sent"`
-	Received TimeStruct `json:"received"`
+type Chain_delay struct {
+	Name     string `json:"tagbackend"`
+	Sent     Time   `json:"sent"`
+	Received Time   `json:"received"`
 }
