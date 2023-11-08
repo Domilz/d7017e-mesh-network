c5af6fe096ff3c499d24cc8c96cec06a39ae3366
Domilz <Daniel.kallberg@hotmail.com>
2023-11-08 12:29:01 +0100

    Added universal main function with args

--- lib/main.go
+++ lib/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Domilz/d7017e-mesh-network/pkg/backend"
+	grpc "github.com/Domilz/d7017e-mesh-network/pkg/protocol/grpc/client"
+)
+
 func main() {
-	// Main main function
+	// Main function, add more cases for main functions of packages when defined.
+	args := os.Args[1:]
+	switch args[0] {
+	case "backend":
+		log.Println("Starting Backend")
+		backend.Main()
+	case "grpc":
+		log.Println("Starting GRPC Client")
+		grpc.Main()
+	default:
+		log.Println("No Main function for given arg")
+		return
+	}
 }
